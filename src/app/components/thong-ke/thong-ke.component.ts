1070c3bc6d8017c07f25ba5680a6d0f106a63429
MasterCloudes <datttph39843@fpt.edu.vn>
2025-02-27 04:18:33 +0700

    suathongke

--- src/app/components/thong-ke/thong-ke.component.ts
+++ src/app/components/thong-ke/thong-ke.component.ts
@@ -8,14 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './thong-ke.component.css'
 })
 export class ThongKeComponent {
-  // Ví dụ các biến ngModel để thao tác form
+
   startDate: string = '';
   endDate: string = '';
   selectedCinema: string = 'Cinema Hà Nội';
   timeFilter: string = 'Lọc theo 1 tháng';
   selectedMovie: string = '';
 
-  // Hàm click nút "THỐNG KÊ"
   handleThongKe() {
     console.log('Xử lý thống kê với:', {
       startDate: this.startDate,
@@ -24,6 +23,5 @@ export class ThongKeComponent {
       timeFilter: this.timeFilter,
       selectedMovie: this.selectedMovie
     });
-    // Thêm logic gọi API hoặc xử lý dữ liệu tại đây (nếu cần)
   }
 }
