e076f9d0c5109d8b77189789c79828d35266a2df
MasterCloudes <datttph39843@fpt.edu.vn>
2025-03-31 00:44:47 +0700

    TODO: chỉnh sửa nhẹ giao diện layout và cho nút đăng xuất hoạt động cùng với chỉnh sửa chút giao diện đăng nhập và thêm ảnh

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../shared/services/user.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  standalone: false,
-
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrls: ['./layout.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule]
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  currentUser: any;
+  
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    // Lấy thông tin người dùng đã đăng nhập
+    this.currentUser = this.userService.getCurrentUser();
+  }
+
+  logout(): void {
+    // Gọi phương thức đăng xuất từ UserService
+    this.userService.logout();
+  }
 }
