dec83dfae0eb787d9093f278ee2b0d02e9cc02a6
MasterCloudes <datttph39843@fpt.edu.vn>
2025-03-31 01:45:12 +0700

    TODO: chỉnh sửa layout có khung cố định và giao diện thể loại phim và phân trang

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../shared/services/user.service';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout',
@@ -12,14 +13,27 @@ import { RouterModule } from '@angular/router';
 })
 export class LayoutComponent implements OnInit {
   currentUser: any;
+  currentRoute: string = '';
   
-  constructor(private userService: UserService) {}
-
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+  
   ngOnInit(): void {
     // Lấy thông tin người dùng đã đăng nhập
     this.currentUser = this.userService.getCurrentUser();
+    
+    // Track current route for navigation highlighting
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const url = event.urlAfterRedirects;
+        this.currentRoute = url.split('/').pop();
+      });
   }
-
+  
+  // Xử lý logout
   logout(): void {
     // Gọi phương thức đăng xuất từ UserService
     this.userService.logout();
