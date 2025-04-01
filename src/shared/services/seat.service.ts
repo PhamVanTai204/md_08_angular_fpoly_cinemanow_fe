b9f69de188c3b1e753a5ae0344eebe93a1439855
PhamVanTai204 <taipv204@gmail.com>
2025-04-02 00:08:30 +0700

    fig lỗi ko lên màn Seat

--- src/shared/services/seat.service.ts
+++ src/shared/services/seat.service.ts
@@ -9,8 +9,29 @@ import { ISeatDto, SeatDto } from "../dtos/seatDto.dto";
 })
 export class SeatService {
     private apiUrlGetbyrom = "http://127.0.0.1:3000/cinema/rooms/seats"; // Base URL
+    private apiUrlAddMultiple = "http://127.0.0.1:3000/seats/addmuti"; // URL để tạo hàng loạt ghế
 
     constructor(private http: HttpClient) { }
+    // Phương thức thêm nhiều ghế
+    addMultipleSeats(room_id: string, rows: number, cols: number, seat_status: string, seat_type: string, price_seat: number): Observable<any> {
+        const data = {
+            room_id: room_id,
+            rows: rows,
+            cols: cols,
+            seat_status: seat_status,
+            seat_type: seat_type,
+            price_seat: price_seat
+        };
+
+        return this.http.post(this.apiUrlAddMultiple, data)
+            .pipe(
+                map(response => {
+                    // Xử lý phản hồi từ API nếu cần
+                    return response;
+                }),
+                catchError(this.handleError)
+            );
+    }
     getSeatByRoomId(roomId: string): Observable<SeatDto[]> {
         return this.http.get<{ code: number, error: any, data: ISeatDto[] }>(`${this.apiUrlGetbyrom}/${roomId}`)
             .pipe(
