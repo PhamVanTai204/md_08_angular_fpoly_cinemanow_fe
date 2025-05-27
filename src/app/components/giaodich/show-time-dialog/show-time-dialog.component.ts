e02ac0e4ceeab5a6c8641d2329904a7b2e98f2ac
PhamVanTai204 <taipv204@gmail.com>
2025-05-27 19:05:12 +0700

    hiện showtime theo ngày và theo rạp

--- src/app/components/giaodich/show-time-dialog/show-time-dialog.component.ts
+++ src/app/components/giaodich/show-time-dialog/show-time-dialog.component.ts
@@ -3,6 +3,7 @@ import { CinemasService } from '../../../../shared/services/cinemas.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { CinemaDto } from '../../../../shared/dtos/cinemasDto.dto';
 import { Router } from '@angular/router';
+import { ShowtimeDateGroup, ShowtimesByMovieAndCinemaResponse, ShowtimesService } from '../../../../shared/services/showtimes.service';
 
 @Component({
   selector: 'app-show-time-dialog',
@@ -16,17 +17,61 @@ export class ShowTimeDialogComponent implements OnInit {
   isLoading: boolean = false;
   cinemas: CinemaDto[] = [];
   error: string | null = null;
-
+  idCinema: string = '';
+  showtimesData: ShowtimeDateGroup[] = [];
   ngOnInit(): void {
-    this.loadCinemas(this.id)
+    // Lấy userCinemaId từ localStorage
+    this.idCinema = localStorage.getItem('userCinemaId') || '';
+    if (this.id) {
+      this.loadShowtimesByMovieAndCinema(this.id, this.idCinema)
+    } else {
+      this.error = 'Không tìm thấy userCinemaId trong localStorage';
+    }
+    this.loadShowtimesByMovieAndCinema(this.id, this.idCinema)
+
   }
   constructor(
     private cinemasService: CinemasService,
+    private showtimesService: ShowtimesService,
+
     private _modalService: BsModalService,
     private router: Router
 
 
   ) { }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('vi-VN', {
+      weekday: 'long',
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
+  }
+
+  loadShowtimesByMovieAndCinema(movieId: string, cinemaId: string): void {
+    this.isLoading = true;
+    this.error = null;
+
+    this.showtimesService.getShowtimesByMovieAndCinema(movieId, cinemaId).subscribe({
+      next: (response: ShowtimesByMovieAndCinemaResponse) => {
+        if (response.code === 200 && response.data) {
+          this.showtimesData = response.data;
+        } else {
+          this.error = 'Không có dữ liệu suất chiếu';
+        }
+        console.log(this.showtimesData);
+
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Đã xảy ra lỗi khi tải suất chiếu';
+        this.isLoading = false;
+        console.error('Error loading showtimes:', err);
+      }
+    });
+  }
   closeModal() {
     this._modalService.hide();
   }
